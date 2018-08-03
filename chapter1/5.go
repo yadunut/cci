559eed0a4f879f7ab9233b99403600312b1f3972
Yadunand Prem <yadunut@gmail.com>
2018-08-03 19:36:47 +0800

    Add Benchmark and optimised qn 5

--- chapter1/5.go
+++ chapter1/5.go
@@ -1,32 +1,31 @@
 package chapter1
 
-import (
-	"fmt"
-	"unicode/utf8"
-)
-
 // CompressString compresses the string and returns the compressed string OR returns the original if it cannot be compressed
 func CompressString(s string) string {
 	var output string
 	var curChar rune
 	var count int
-	curChar, _ = utf8.DecodeRuneInString(s)
+	for _, v := range s {
+		curChar = v
+		break
+	}
 
 	for _, v := range s {
 		if v == curChar {
 			count++
 			continue
 		} else {
-			output += fmt.Sprintf("%s%d", string(curChar), count)
+			output += string(curChar)
+			output += string(count)
 			count = 1
 			curChar = v
 		}
 	}
-	output += fmt.Sprintf("%s%d", string(curChar), count)
+	output += string(curChar)
+	output += string(count)
 
 	if len(output) == len(s) {
 		return s
 	}
-
 	return output
 }
--- chapter1/5_test.go
+++ chapter1/5_test.go
@@ -2,27 +2,35 @@ package chapter1
 
 import "testing"
 
+type args struct {
+	s string
+}
+
+var tests = []struct {
+	name string
+	args args
+	want string
+}{
+	{args: args{"aaaa"}, want: "a4"},
+	{args: args{"bbbb"}, want: "b4"},
+	{args: args{"cccc"}, want: "c4"},
+	{args: args{"dddd"}, want: "d4"},
+	{args: args{"aaaabbbbccccdddd"}, want: "a4b4c4d4"},
+	{args: args{"aabbccdd"}, want: "aabbccdd"},
+}
+
 func TestCompressString(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{args: args{"aaaa"}, want: "a4"},
-		{args: args{"bbbb"}, want: "b4"},
-		{args: args{"cccc"}, want: "c4"},
-		{args: args{"dddd"}, want: "d4"},
-		{args: args{"aaaabbbbccccdddd"}, want: "a4b4c4d4"},
-		{args: args{"aabbccdd"}, want: "aabbccdd"},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := CompressString(tt.args.s); got != tt.want {
 				t.Errorf("CompressString(\"%v\") = %v, want %v", tt.args.s, got, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkCompressString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CompressString("aaaabbbbccccdddd")
 	}
 }
