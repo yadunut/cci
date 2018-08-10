1c18123d9a35f09bfb4305056748aaa921a8bcb9
Yadunand Prem <yadunut@gmail.com>
2018-08-11 00:44:36 +0800

    Add all qns for chapter 1

---
+++ chapter1/7.go
@@ -0,0 +1,4 @@
+package chapter1
+
+// Write an algorithm such that if an element in a MxN matrix is 0, its entire
+// row and column are set to 0
---
+++ chapter1/8.go
@@ -0,0 +1,6 @@
+package chapter1
+
+// Assume you have a method isSubstring ehich checks if one word is a substring
+// of another. Given 2 strings s1 and s2, write code to check if s2 is a
+// rotation of s1 using only 1 call to isSubstring
+// E.g. "waterbottle" is a rotation of "erbottlewat"
